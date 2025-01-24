e24a373b6e82eea57981255972293f7c4dd51e21
Chavanvit <chavanvit.n@gmail.com>
2025-01-24 14:48:19 +0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/socket.io/:path*",
-        destination:
-          "https://merrymatchsocket-production.up.railway.app/socket.io/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
